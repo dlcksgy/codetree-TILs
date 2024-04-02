78aded0be3d7c0b9d195bdb82cc7d8bd396eee9d
dlcksgy <hyo517@naver.com>
2024-04-02 13:07:28 +0900

    Title: 포탑 부수기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240402/포탑 부수기/destroy-the-turret.cpp
+++ 240402/포탑 부수기/destroy-the-turret.cpp
@@ -235,9 +235,9 @@ int main() {
     
     for(int i = 0; i < N; i++){
         for(int j = 0; j < M; j++){
-            //cout << TURRETS[i][j] << "/";
+            cout << TURRETS[i][j] << "/";
         }
-        //cout << endl;
+        cout << endl;
     }
     pair<int,int> answer = find_max(TURRETS);
     cout << TURRETS[answer.second][answer.first] << "\n";

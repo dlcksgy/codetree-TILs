bfc72148eba653b368c0a786b7c3a3ea436e28f2
dlcksgy <hyo517@naver.com>
2024-04-04 23:45:44 +0900

    Title: 트리의 부모 노드, Time: 1072ms, Memory: 21MB, Status: Time Limit Exceed - CodeTree

--- 240404/트리의 부모 노드/parent-node-of-the-tree.cpp
+++ 240404/트리의 부모 노드/parent-node-of-the-tree.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_set>
+#include <unordered_map>
 #include <deque>
 
 using namespace std;
@@ -9,13 +10,21 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int N;
     cin >> N;
-    vector<vector<int>> tree_graph(N+1,vector<int>(N+1,0));
+    unordered_map<int,unordered_set<int>> tree_graph;
     vector<int> parent(N+1,0);
     for(int i = 0; i < N-1; i++){
         int t1,t2;
         cin >> t1 >> t2;
-        tree_graph[t1][t2] = 1;
-        tree_graph[t2][t1] = 1;
+        if(tree_graph.find(t1) == tree_graph.end()){
+            tree_graph.emplace(t1,unordered_set<int>({t2}));
+        }else{
+            tree_graph[t1].insert(t2);
+        }
+        if(tree_graph.find(t2) == tree_graph.end()){
+            tree_graph.emplace(t2,unordered_set<int>({t1}));
+        }else{
+            tree_graph[t2].insert(t1);
+        }
     }
     unordered_set<int> visited;
     deque<int> Q;
@@ -25,7 +34,7 @@ int main() {
         int p_node = Q.front();
         Q.pop_front();
         for(int i = 0; i < N+1; i++){
-            if(tree_graph[p_node][i] == 1 && visited.find(i) == visited.end()){
+            if(tree_graph[p_node].find(i) != tree_graph[p_node].end() && visited.find(i) == visited.end()){
                 Q.push_back(i);
                 visited.insert(i);
                 parent[i] = p_node;
